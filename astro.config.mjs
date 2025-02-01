bde1f4bf10e50223ff5eabc765315408a085191b
eduwill <eduwill100@gmail.com>
2025-01-31 23:51:39 -0300

    feat(server): add vercel para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [
     react(), 
@@ -9,4 +11,6 @@ export default defineConfig({
       applyBaseStyles: false,
     })
   ],
+
+  adapter: vercel(),
 });
